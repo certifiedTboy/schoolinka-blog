45a47fb0134c1816c58d27d6a7ced563b795881d
certifiedTboy <79862233+certifiedTboy@users.noreply.github.com>
2023-09-04 23:06:51 +0100

    update workflow

---
+++ src/database/models/Blog.js
@@ -0,0 +1,43 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var sequelize_1 = require("sequelize");
+var dbconfig_1 = require("../dbconfig");
+var Blog = /** @class */ (function (_super) {
+    __extends(Blog, _super);
+    function Blog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Blog;
+}(sequelize_1.Model));
+Blog.init({
+    id: {
+        type: sequelize_1.DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    description: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+    content: {
+        type: sequelize_1.DataTypes.STRING,
+    },
+}, { sequelize: dbconfig_1.sequelize });
+exports.default = Blog;
